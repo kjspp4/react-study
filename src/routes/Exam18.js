d3ca0cc3ac62f5645c0a3ab0d3f50bc554bf6e67
kjspp4 <kjspp4@gmail.com>
2020-10-29 14:49:17 +0900

    가위바위보

--- src/routes/Exam18.js
+++ src/routes/Exam18.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import '../index.css'
 const Exam18 = () => {
+
+    const [result, setResult] = useState('');
+    const [score, setScore] = useState(0);
+    const [imgCoord, setImgCoord] = useState([]);
+
+
     return (
         <div>
             가위바위보
